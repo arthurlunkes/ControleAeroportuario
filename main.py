0ebdd517e01bcca3957d1738e39e786d7d53c922
R-DaRosa-Antonio <Rodrigues.309.Antonio@gmai.com>
2023-10-27 19:34:40 -0300

    rota_de_delete

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,  HTTPException
+from datetime import datetime
 
 app = FastAPI()
+
+class Aeronave:
+    id: int
+    nome: str
+    horario_chegada: datetime
+    horario_partida: datetime
+
+# Dicionário para armazenar as aeronaves em memória
+aeronaves = {}
 
 @app.get()
 
@@ -10,4 +20,15 @@ app = FastAPI()
 # Arthur
 @app.patch()
 
+#Antonio
 @app.delete()
+
+# Rota para excluir uma aeronave com base no ID
+@app.delete("/aeronaves/{aeronave_id}")
+def excluir_aeronave(aeronave_id: int):
+    # Verifica se a aeronave existe
+    if aeronave_id not in aeronaves:
+        raise HTTPException(status_code=404, detail="Aeronave não encontrada")
+    
+    aeronave_excluida = aeronaves.pop(aeronave_id)
+    return aeronave_excluida
